ac663bdb9a66771d3d04748d8ca4d5d304074a60
Shamil Rafeeque <shamilrafeeque04@gmai.com>
2022-11-25 19:20:12 +0530

    added gitignore

--- django_lms/lms_api/lms_api/settings.py
+++ django_lms/lms_api/lms_api/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework_simplejwt',
     'payments',
+    'rest_framework_swagger',
   
 ]
 
--- django_lms/lms_api/lms_api/urls.py
+++ django_lms/lms_api/lms_api/urls.py
@@ -7,6 +7,10 @@ from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('main.urls')),
@@ -15,5 +19,6 @@ urlpatterns = [
     # path('api/token/refresh/',TokenRefreshView.as_view(), name='token_refresh'),
     path('api/acounts/',include('accounts.urls')), 
     path('razorpay/', include('payments.urls')),
+    path('documentation/',schema_view),
 
 ]+static(settings.STATIC_URL,document_root= settings.STATIC_ROOT)
